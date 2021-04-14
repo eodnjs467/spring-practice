7dc675a6ad5ad5f126aa630258b1d4ba22423755
daewon <sponjjanc@naver.com>
2021-04-14 14:13:32 +0900

    스프링 부트 AWS로 웹서비스 구현 // JPA, H2->MySQL

---
+++ src/test/java/com/example/springpractice/domain/posts/PostsRepositoryTest.java
@@ -0,0 +1,46 @@
+package com.example.springpractice.domain.posts;
+
+import org.aspectj.lang.annotation.After;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class PostsRepositoryTest {
+
+    @Autowired
+    PostsRepository postsRepository;
+
+    @AfterEach
+    public void cleanup(){
+        postsRepository.deleteAll();
+    }
+
+    @Test
+    public void 게시글저장_불러오기(){
+        //given
+        String title = "test";
+        String content = "test_content";
+
+        postsRepository.save(Posts.builder()
+                .title(title)
+                .content(content)
+                .author("Kim@gmail.com")
+                .build());
+
+        //when
+        List<Posts> postsList = postsRepository.findAll();
+
+        //then
+        Posts posts = postsList.get(0);
+        assertThat(posts.getTitle()).isEqualTo(title);
+        assertThat(posts.getContent()).isEqualTo(content);
+    }
+
+}
