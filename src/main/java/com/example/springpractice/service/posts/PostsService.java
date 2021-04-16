2bd75ac34a7cdff29aa5e249c0852cb44b4dae20
daewon <sponjjanc@naver.com>
2021-04-16 23:07:58 +0900

    mustache 화면구성(1)

--- src/main/java/com/example/springpractice/service/posts/PostsService.java
+++ src/main/java/com/example/springpractice/service/posts/PostsService.java
@@ -2,12 +2,16 @@ package com.example.springpractice.service.posts;
 
 import com.example.springpractice.domain.posts.Posts;
 import com.example.springpractice.domain.posts.PostsRepository;
+import com.example.springpractice.web.dto.PostsListResponseDto;
 import com.example.springpractice.web.dto.PostsResponseDto;
 import com.example.springpractice.web.dto.PostsSaveRequestDto;
 import com.example.springpractice.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 
 @RequiredArgsConstructor
@@ -30,10 +34,17 @@ public class PostsService {
         return id;
     }
 
-    public PostsResponseDto findById (Long id) {
+    public PostsResponseDto findById(Long id) {
         Posts entity = postsRepository.findById(id)
                 .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id = " + id));
 
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc(){
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
     }
 }
