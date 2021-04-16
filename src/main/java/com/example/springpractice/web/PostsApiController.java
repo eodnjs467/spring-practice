428c3d74a71437df1dfe6e3612b0fa3ba84193de
daewon <sponjjanc@naver.com>
2021-04-16 18:14:36 +0900

    등록/수정/조회 API 만들기(2)

--- src/main/java/com/example/springpractice/web/PostsApiController.java
+++ src/main/java/com/example/springpractice/web/PostsApiController.java
@@ -2,17 +2,28 @@ package com.example.springpractice.web;
 
 
 import com.example.springpractice.service.posts.PostsService;
+import com.example.springpractice.web.dto.PostsResponseDto;
 import com.example.springpractice.web.dto.PostsSaveRequestDto;
+import com.example.springpractice.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
 public class PostsApiController {
 
     private final PostsService postsService;
+
+
+    @PutMapping("/api/v1/posts/{id}")
+    public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto) {
+        return postsService.update(id, requestDto);
+    }
+
+    @GetMapping("/api/v1/posts/{id}")
+    public PostsResponseDto findById(@PathVariable Long id) {
+        return postsService.findById(id);
+    }
 
     @PostMapping("/api/v1/posts")
     public Long save(@RequestBody PostsSaveRequestDto requestDto) {
